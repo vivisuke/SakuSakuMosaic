c22619fa018f96ceadef0e9fc13d273943fa6155
vivisuke <ntsuda@master.email.ne.jp>
2021-08-15 08:54:39 +0900

    0007 ◎ C 08/12 08/15 0.001│ToDo│Global.gd 追加

---
+++ SakuSakuMosaic/Global.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+const SCREEN_WIDTH = 500.0
+const SCREEN_HEIGHT = 800.0
+const BOARD_WIDTH = 480.0
+const BOARD_HEIGHT = BOARD_WIDTH
+
+const N_IMG_CELL_HORZ = 15		# 画像 セル数
+const N_IMG_CELL_VERT = 15		# 画像 セル数
+const CELL_WIDTH = BOARD_WIDTH / N_IMG_CELL_HORZ
+
+func _ready():
+	pass # Replace with function body.
