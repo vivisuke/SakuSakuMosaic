aafd0b975248b447c5b6f8c7970e36a3cba033dd
vivisuke <ntsuda@master.email.ne.jp>
2021-08-15 09:39:14 +0900

    0009 ◎ C 08/12 08/15 0.001│ToDo│マウス押下ハンドラ追加

---
+++ SakuSakuMosaic/MainScene.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+onready var g = get_node("/root/Global")
+
+const BLACK = 0
+const CROSS = 1
+const UNKNOWN = -1
+
+var crnt_color = 3
+
+func _ready():
+	pass # Replace with function body.
+func posToXY(pos):
+	var xy = $BoardBG/TileMap.world_to_map(pos - $BoardBG/TileMap.global_position)
+	if xy.x < 0 || xy.x >= g.N_IMG_CELL_HORZ || xy.y < 0 || xy.y >= g.N_IMG_CELL_VERT:
+		return Vector2(-1, -1)
+	return xy
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		var xy = posToXY(event.position)
+		print(xy)
+		if xy.x >= 0:
+			cell_pressed(xy.x, xy.y)
+func cell_pressed(x, y):
+	#print("(", x, ", ", y, ")")
+	if $BoardBG/TileMap.get_cell(x, y) == UNKNOWN:
+		$BoardBG/TileMap.set_cell(x, y, BLACK)
+	elif $BoardBG/TileMap.get_cell(x, y) == BLACK:
+		$BoardBG/TileMap.set_cell(x, y, CROSS)
+	else:
+		$BoardBG/TileMap.set_cell(x, y, UNKNOWN)
